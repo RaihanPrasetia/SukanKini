0f7c80e40c369753d4d602772833dc6e9d9ed59f
Get's <91049327+Me291@users.noreply.github.com>
2024-11-01 14:49:17 +0700

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Header from './components/Header.jsx';
-import ThemeToggleButton from './components/ThemeToggleButton';
 import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
-    <div className='bg-white dark:bg-gray-800 min-h-screen'>
+    <div className="bg-white dark:bg-gray-800 min-h-screen">
       <Router>
-        <Header />
-        <div className="flex justify-center">
-          <ThemeToggleButton />
-        </div>
         <AppRoutes />
       </Router>
     </div>
